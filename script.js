3bdcce9faef1caa049dd4da31ad6ef67b36d235f
sohagmnr <sohagm8192@gmail.com>
2024-09-24 03:03:24 -0700

    fixed small problem in javascript and html

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 function donate() {
     let myBalance = parseFloat(document.getElementById("myBalance").innerText);
-    let donations = [];
+    let donations = [ ];
     for (let i= 1;i <=3; i++) {
         let donation = parseFloat(document.getElementById(`charity${i}`).value) || 0;
         donations.push(donation);
@@ -9,7 +9,7 @@ function donate() {
 
     let totalDonation =  donations.reduce((sum, donation) =>sum + donation, 0);
     if (totalDonation> myBalance) {
-        alert("Sorry, you don't have enough balance to donate this amount.");
+        alert("Sorry, you don't have enough balance");
         return;
     }
 
@@ -22,39 +22,38 @@ function donate() {
         document.getElementById(`charity${i}`).value= '';
     }
 
-    document.getElementById("thankYouModal").classList.remove("hidden");
+    document.getElementById("congoModal").classList.remove("hidden");
     createHistoryCard(donations);
 }
 
 
 document.getElementById("closeModal").addEventListener("click", function () {
-    document.getElementById("thankYouModal").classList.add("hidden");
+    document.getElementById("congoModal").classList.add("hidden");
 });
 
 
 
-function showDonations() {
-    document.getElementById("donationSection").style.display = "block";
+function viewDono() {
+    document.getElementById("dntSec").style.display = "block";
     document.getElementById("historySection").style.display = "none";
 
 
-    document.getElementById("donationButton").classList.add("bg-btnColor");
-    document.getElementById("donationButton").classList.remove("text-black");
-    document.getElementById("historyButton").classList.add("text-customGray");
-    document.getElementById("historyButton").classList.remove("bg-btnColor");
+    document.getElementById("dntBtn").classList.add("bg-btnColor");
+    document.getElementById("historyBtn").classList.add("text-customGray");
+    document.getElementById("historyBtn").classList.remove("bg-btnColor");
 }
 
 
 
-function showHistory() {
-    document.getElementById("donationSection").style.display = "none";
+function viewHistory() {
+    document.getElementById("dntSec").style.display = "none";
     document.getElementById("historySection").style.display = "block";
 
-    document.getElementById("historyButton").classList.add("bg-btnColor");
-    document.getElementById("historyButton").classList.remove("text-customGray");
-    document.getElementById("donationButton").classList.add("text-black");
-    document.getElementById("donationButton").classList.remove("bg-btnColor");
-    document.getElementById("donationButton").classList.add("bg-bCustomGray");
+    document.getElementById("historyBtn").classList.add("bg-btnColor");
+    document.getElementById("historyBtn").classList.remove("text-customGray");
+    document.getElementById("historyBtn").classList.add("text-black");
+    document.getElementById("dntBtn").classList.remove("bg-btnColor");
+    document.getElementById("dntBtn").classList.add("bg-bCustomGray");
 }
 
 
@@ -83,6 +82,6 @@ function createHistoryCard(donations) {
 
 
 
-document.addEventListener("DOMContentLoaded", showDonations);
+document.addEventListener("DOMContentLoaded", viewDono);
 
 
